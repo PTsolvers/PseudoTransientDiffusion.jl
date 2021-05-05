d54829bc081bdd9aa4d688975b506b01f6dfc2ba
Ludovic Räss <ludovic.rass@gmail.com>
2021-05-05 17:37:22 +0200

    initial codes

--- README.md
+++ README.md
@@ -7,16 +7,28 @@ This repository contains various diffusion solvers to examplify, test and asses 
 💡 Link to the [Overleaf draft](https://www.overleaf.com/project/5ff83a57858b372f63143b8e)
 
 ## Content
-* [References](#references)
 * [The diffusion equation](#the-diffusion-equation)
+* [Scripts](#scripts)
+* [References](#references)
 
 ## The diffusion equation
-
+In this study we will use the (non-linear) diffusion (reaction) equation in 1D, 2D (and 3D):
 ```julia
 qHx   = -D*dH/dx
 qHy   = -D*dH/dy
 dH/dt = -(dqHx/dx + dqHy/dy) + M
 ```
 
+## Scripts
+- The `diff_1D_xx.jl` scripts are used to evaluate the iteration scaling with varying numerical grid resolution.
+
+- The `diff_2D_nonlin_perfcompare_xx.jl` scripts are used to generate the following figure (for [ParallelStencil.jl] repo):
+
+![](docs/perf_ps2.png)
+
+
 ## References
 [Frankel, S. P. (1950). Convergence rates of iterative treatments of partial differential equations, Mathe. Tables Other Aids Comput., 4, 65–75.](https://doi.org/10.2307/2002770)
+
+
+[ParallelStencil.jl]: https://github.com/omlins/ParallelStencil.jl
---
+++ scripts/diff_1D_nonlin.jl
@@ -0,0 +1,71 @@
+const USE_GPU = false
+using ParallelStencil
+using ParallelStencil.FiniteDifferences1D
+@static if USE_GPU
+    @init_parallel_stencil(CUDA, Float64, 1)
+else
+    @init_parallel_stencil(Threads, Float64, 1)
+end
+using Plots, Printf, LinearAlgebra
+
+macro H3() esc(:( @av(H)*@av(H)*@av(H) )) end
+@parallel function compute_flux!(qHx, H, D, dx)
+    @all(qHx) = -@H3()*@d(H)/dx
+    return
+end
+
+@parallel function compute_rate!(ResH, dHdt, H, Hold, qHx, dt, damp, dx)
+    @all(ResH) = -(@inn(H) - @inn(Hold))/dt -@d(qHx)/dx
+    @all(dHdt) = @all(ResH) + damp*@all(dHdt)
+    return
+end
+
+macro dtau() esc(:( 1.0/(1.0/(dx*dx/@H3()/2.1) + 1.0/dt) )) end
+@parallel function compute_update!(H, dHdt, dt, dx)
+    @inn(H) = @inn(H) + @dtau()*@all(dHdt)
+    return
+end
+
+@views function diffusion_1D()
+    # Physics
+    lx     = 10.0       # domain size
+    D      = 1.0        # diffusion coefficient
+    ttot   = 1.0        # total simulation time
+    dt     = 0.2        # physical time step
+    # Numerics
+    nx     = 256        # numerical grid resolution
+    tol    = 1e-6       # tolerance
+    itMax  = 1e5        # max number of iterations
+    damp   = 1-45/nx    # damping (this is a tuning parameter, dependent on e.g. grid resolution)
+    # Derived numerics
+    dx     = lx/nx      # grid size
+    xc     = LinRange(dx/2, lx-dx/2, nx)
+    # Array allocation
+    qHx    = @zeros(nx-1)
+    dHdt   = @zeros(nx-2)
+    ResH   = @zeros(nx-2)
+    # Initial condition
+    H0     = Data.Array( exp.(-(xc.-lx/2).^2) )
+    Hold   = @ones(nx).*H0
+    H      = @ones(nx).*H0
+    t = 0.0; it = 0; ittot = 0
+    # Physical time loop
+    while t<ttot
+        iter = 0; err = 2*tol
+        # Pseudo-transient iteration
+        while err>tol && iter<itMax
+            @parallel compute_flux!(qHx, H, D, dx)
+            @parallel compute_rate!(ResH, dHdt, H, Hold, qHx, dt, damp, dx)
+            @parallel compute_update!(H, dHdt, dt, dx)
+            iter += 1; err = norm(ResH)/length(ResH)
+        end
+        ittot += iter; it += 1; t += dt
+        Hold .= H
+    end
+    @printf("Total time = %1.2f, time steps = %d, iterations tot = %d \n", round(ttot, sigdigits=2), it, ittot)
+    # Visualise
+    plot(xc, Array(H0), linewidth=3); display(plot!(xc, Array(H), legend=false, framestyle=:box, linewidth=3, xlabel="lx", ylabel="H", title="nonlinear diffusion (nt=$it, iters=$ittot)"))
+    return
+end
+
+diffusion_1D()
