ccc48458c091eb26dd8851278a24e4939bd92da4
Ludovic Räss <ludovic.rass@gmail.com>
2021-08-06 00:26:00 +0200

    add default to const

--- scripts/diff_1D_lin3.jl
+++ scripts/diff_1D_lin3.jl
@@ -1,8 +1,8 @@
-const USE_GPU = parse(Bool, ENV["USE_GPU"])
-const do_viz  = parse(Bool, ENV["DO_VIZ"])
-const do_save = parse(Bool, ENV["DO_SAVE"])
-const do_save_viz = parse(Bool, ENV["DO_SAVE_VIZ"])
-const nx = parse(Int, ENV["NX"])
+const USE_GPU = haskey(ENV, "USE_GPU") ? parse(Bool, ENV["USE_GPU"]) : false
+const do_viz  = haskey(ENV, "DO_VIZ")  ? parse(Bool, ENV["DO_VIZ"])  : true
+const do_save = haskey(ENV, "DO_SAVE") ? parse(Bool, ENV["DO_SAVE"]) : false
+const do_save_viz = haskey(ENV, "DO_SAVE_VIZ") ? parse(Bool, ENV["DO_SAVE_VIZ"]) : false
+const nx = haskey(ENV, "NX") ? parse(Int, ENV["NX"]) : 512
 ###
 using ParallelStencil
 using ParallelStencil.FiniteDifferences1D
