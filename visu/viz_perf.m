cdbe5125ed569acc270fb819c24c1b5d6e1aebca
Ludovic Raess <ludovic.rass@gmail.com>
2021-10-19 22:27:32 +0200

    Add Piz Daint single GPU scaling and visu.

--- visu/viz_perf.m
+++ visu/viz_perf.m
@@ -3,8 +3,10 @@ clear
 % load scaling_data
 fid = fopen('../output/out_diff_2D_nonlin3_perf_octo.txt' ,'r'); diff_2D_octo  = fscanf(fid, '%d %d %d %f %f %f %f', [7 Inf]); fclose(fid); % nx ny ittot t_toc A_eff t_it T_eff
 fid = fopen('../output/out_diff_2D_nonlin3_perf_volta.txt','r'); diff_2D_volta = fscanf(fid, '%d %d %d %f %f %f %f', [7 Inf]); fclose(fid); % nx ny ittot t_toc A_eff t_it T_eff
+fid = fopen('../output/out_diff_2D_nonlin3_perf_daint.txt','r'); diff_2D_daint = fscanf(fid, '%d %d %d %f %f %f %f', [7 Inf]); fclose(fid); % nx ny ittot t_toc A_eff t_it T_eff
 fid = fopen('../output/out_diff_3D_nonlin3_perf_octo.txt' ,'r'); diff_3D_octo  = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 fid = fopen('../output/out_diff_3D_nonlin3_perf_volta.txt','r'); diff_3D_volta = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
+fid = fopen('../output/out_diff_3D_nonlin3_perf_daint.txt','r'); diff_3D_daint = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 fid = fopen('../../PseudoTransientStokes/output/out_Stokes3D_ve3_perf_octo.txt' ,'r'); stokes_3D_octo  = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 fid = fopen('../../PseudoTransientStokes/output/out_Stokes3D_ve3_perf_volta.txt','r'); stokes_3D_volta = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 
@@ -13,14 +15,17 @@ fid = fopen('../output/out_diff_3D_nonlin3_multixpu_perf_volta.txt','r'); diff_3
 fid = fopen('../../PseudoTransientStokes/output/out_Stokes3D_ve3_xpu_perf_octo.txt' ,'r'); stokes_3D_mxpu_octo  = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 fid = fopen('../../PseudoTransientStokes/output/out_Stokes3D_ve3_xpu_perf_volta.txt','r'); stokes_3D_mxpu_volta = fscanf(fid, '%d %d %d %d %d %f %f %f %f', [9 Inf]); fclose(fid); % np nx ny nz ittot t_toc A_eff t_it T_eff
 
-nrep = 5; % number of repetitions of the experiment
+nrep  = 5; % number of repetitions of the experiment
+nrep2 = 2; % number of repetitions of the experiment
 my_type = "my_max";
 % my_type = "my_mean";
 
 diff_2D_octo_2  = average_exp(diff_2D_octo, nrep, my_type);
 diff_2D_volta_2 = average_exp(diff_2D_volta, nrep, my_type);
+diff_2D_daint_2 = average_exp(diff_2D_daint, nrep2, my_type);
 diff_3D_octo_2  = average_exp(diff_3D_octo, nrep, my_type);
 diff_3D_volta_2 = average_exp(diff_3D_volta, nrep, my_type);
+diff_3D_daint_2 = average_exp(diff_3D_daint, nrep2, my_type);
 diff_3D_mxpu_octo_2  = average_exp(diff_3D_mxpu_octo, nrep, my_type);
 diff_3D_mxpu_volta_2 = average_exp(diff_3D_mxpu_volta, nrep, my_type);
 
@@ -31,6 +36,7 @@ stokes_3D_mxpu_volta_2 = average_exp(stokes_3D_mxpu_volta, nrep, my_type);
 
 T_peak_volta = 840;
 T_peak_octo  = 254;
+T_peak_daint = 561;
 
 FS = 20;
 mylim = [0 870];
@@ -39,18 +45,20 @@ ylab = 790;
 mylim2 = [0.89 1.01];
 ylab2 = 0.9;
 
-fig1 = 0;
-fig2 = 1;
+fig1 = 1;
+fig2 = 0;
 
 %%
 if fig1==1
 figure(1),clf,set(gcf,'color','white','pos',[1400 10 800 400])
 sp1 = subplot(121);
-semilogx(diff_2D_octo_2(1,:),diff_2D_octo_2(end,:), '-o', ...
-     diff_2D_volta_2(1,:),diff_2D_volta_2(end,:), '-o', ...
-     diff_2D_octo_2(1,:),T_peak_octo*ones(size(diff_2D_octo_2(1,:))), 'k--',...
-     diff_2D_octo_2(1,:),T_peak_volta*ones(size(diff_2D_octo_2(1,:))),'k-.', ...
-     'linewidth',3, 'MarkerFaceColor','k'), axis square, set(gca, 'fontsize',FS, 'linewidth',1.4)
+%diff_2D_octo_2(1,:),diff_2D_octo_2(end,:), '-o', ...
+%diff_2D_octo_2(1,:),T_peak_octo*ones(size(diff_2D_octo_2(1,:))), 'k--',...
+semilogx(diff_2D_volta_2(1,:),diff_2D_volta_2(end,:), '-o', ...
+         diff_2D_daint_2(1,:),diff_2D_daint_2(end,:), '-o', ...
+         diff_2D_octo_2(1,:),T_peak_volta*ones(size(diff_2D_octo_2(1,:))),'k-.', ...
+         diff_2D_octo_2(1,:),T_peak_daint*ones(size(diff_2D_octo_2(1,:))),'k--', ...
+         'linewidth',3, 'MarkerFaceColor','k'), axis square, set(gca, 'fontsize',FS, 'linewidth',1.4)
 title({'2D nonlinear'; 'diffusion'},'fontsize',FS-2)
  ylabel({' ';'\bf{T_{eff} [GB/s]}'}, 'fontsize',FS)
 ylim(mylim)
@@ -62,13 +70,15 @@ text(40,ylab,'a)','fontsize',FS+2,'fontname','Courier')
 
 
 sp2 = subplot(122);
-semilogx(diff_3D_octo_2(2,:),diff_3D_octo_2(end,:), '-o', ...
-     diff_3D_volta_2(2,:),diff_3D_volta_2(end,:), '-o', ...
-     diff_3D_volta_2(2,:),T_peak_octo*ones(size(diff_3D_volta_2(2,:))), 'k--',...
-     diff_3D_volta_2(2,:),T_peak_volta*ones(size(diff_3D_volta_2(2,:))),'k-.', ...
+%diff_3D_octo_2(2,:),diff_3D_octo_2(end,:), '-o', ...
+%diff_3D_volta_2(2,:),T_peak_octo*ones(size(diff_3D_volta_2(2,:))), 'k--',...
+semilogx(diff_3D_volta_2(2,:),diff_3D_volta_2(end,:), '-o', ...
+         diff_3D_daint_2(2,:),diff_3D_daint_2(end,:), '-o', ...
+         diff_3D_volta_2(2,:),T_peak_volta*ones(size(diff_3D_volta_2(2,:))),'k-.', ...
+         diff_3D_volta_2(2,:),T_peak_daint*ones(size(diff_3D_volta_2(2,:))),'k--', ...
      'linewidth',3, 'MarkerFaceColor','k'), axis square, set(gca, 'fontsize',FS, 'linewidth',1.4)
 title({'3D nonlinear'; 'diffusion'},'fontsize',FS-2)
-lg=legend('Titan Xm', 'Tesla V100 SXM2'); set(lg,'box','off')
+lg=legend('Tesla V100 SXM2', 'Tesla P100 PCIe'); set(lg,'box','off')
 ylim(mylim)
 set(gca, 'XTick',diff_3D_volta_2(2,:), 'YTicklabel',[])
 xtickangle(45)
@@ -80,7 +90,7 @@ pos1 = get(sp1,'position'); set(sp1,'position',[pos1(1)*0.97  pos1(2)*1.15 pos1(
 pos2 = get(sp2,'position'); set(sp2,'position',[pos2(1)*0.97  pos2(2)*1.15 pos2(3)*1 pos2(4)*1])
 fig = gcf;
 fig.PaperPositionMode = 'auto';
-% print('fig_perf23D_diff','-dpng','-r300')
+% print('fig_perf23D_diff_2','-dpng','-r300')
 end
 %%
 if fig2==1
